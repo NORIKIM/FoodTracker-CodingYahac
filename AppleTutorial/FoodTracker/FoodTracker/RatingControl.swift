d90264584481fab77e1e64d17a6f3de152138714
김지나 <jinagold.k@gmail.com>
2019-08-11 18:39:10 +0900

    Add New file for implement a custom rating control

---
+++ AppleTutorial/FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by 김지나 on 11/08/2019.
+//  Copyright © 2019 김지나. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
