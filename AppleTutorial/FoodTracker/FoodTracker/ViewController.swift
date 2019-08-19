871603d02c61c075e0982f437d04d2f2f293db82
김지나 <jinagold.k@gmail.com>
2019-08-19 16:16:46 +0900

    Connect the Rating Control to the View Controller

--- AppleTutorial/FoodTracker/FoodTracker/ViewController.swift
+++ AppleTutorial/FoodTracker/FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
