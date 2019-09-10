7e26ff4ab6fa4ac42cb4771d846d6bafada73f1e
김지나 <jinagold.k@gmail.com>
2019-09-10 16:48:00 +0900

    Support Deleting Meals

--- AppleTutorial/FoodTracker/FoodTracker/MealTableViewController.swift
+++ AppleTutorial/FoodTracker/FoodTracker/MealTableViewController.swift
@@ -19,6 +19,9 @@ class MealTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 테이블 뷰 컨트롤러에서 제공하는 edit버튼 사용하기
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         // Load the sample data.
         loadSamplemeal()
     }
@@ -53,25 +56,26 @@ class MealTableViewController: UITableViewController {
     }
  
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+ 
 
     /*
     // Override to support rearranging the table view.
